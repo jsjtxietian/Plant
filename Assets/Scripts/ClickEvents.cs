85782094d47cf756261642004aef316b9e9ae2f1
天 谢 <jsjtxietian@outlook.com>
2018-08-14 13:38:39 +0800

    init scenes

---
+++ Assets/Scripts/ClickEvents.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickEvents : MonoBehaviour {
+
+    public void P01ToP02()
+    {
+        Debug.Log("hello");
+    }
+}
