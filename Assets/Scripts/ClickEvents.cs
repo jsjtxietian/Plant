674efb597594967a22ec8d24e45e7d5ab21dbaaa
天 谢 <jsjtxietian@outlook.com>
2018-08-14 14:59:16 +0800

    more ui

--- Assets/Scripts/ClickEvents.cs
+++ Assets/Scripts/ClickEvents.cs
@@ -1,11 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class ClickEvents : MonoBehaviour {
-
+public class ClickEvents : MonoBehaviour
+{
+#region P01
     public void P01ToP02()
     {
-        Debug.Log("hello");
+        SceneManager.LoadScene("P02");
+    }
+
+#endregion
+
+#region P02
+    public GameObject P2_UI1;
+    public GameObject P2_UI2;
+    public GameObject P2_UI3;
+    public GameObject P2_UI_PlayMovie;
+    public GameObject P2_UI_Next;
+
+
+
+    public void P02_1_2()
+    {
+        P2_UI1.SetActive(false);
+        P2_UI2.SetActive(true);
+    }
+
+    public void P02_2_3()
+    {
+        P2_UI2.SetActive(false);
+        P2_UI3.SetActive(true);
+    }
+
+    public void P02_3_4()
+    {
+        P2_UI3.SetActive(false);
+        P2_UI_PlayMovie.SetActive(true);
+    }
+
+    public void PlayMovie() //P02 teaching movie
+    {
+        
+    }
+
+    public void Replay() // P02 replay teaching movie
+    {
+        
+    }
+
+
+#endregion
+
+
+    public void ToLevelSelection()
+    {
+        SceneManager.LoadScene("LevelSelection");
     }
 }
