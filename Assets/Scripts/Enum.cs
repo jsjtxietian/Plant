7534966e7bb66cc27b67c6627355bd5ba1417d44
天 谢 <jsjtxietian@outlook.com>
2018-08-16 13:48:12 +0800

    scene over

---
+++ Assets/Scripts/Enum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public enum Command
+{
+    None,
+    Up,
+    Down,
+    Left,
+    Right,
+    Pick,
+    Put,
+    Pause
+}
